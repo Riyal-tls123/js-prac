c6bb3668092229a283c643cc6323bdcd307b3827
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-16 05:37:37 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,18 @@
 "use strict";  // treat all JS code as newer ver
 
-alert (3+3)
+// alert (3+3) // we are using jsnode, not browser so this syntax will not work
+
+/*
+types of data types
+number -> 2 to the power 53 obdhi ney
+bigint r o boro input newar jonno kaje lge
+string -> ""
+boolean -> true/false
+null -> standalone value
+undefined -> jar value dewa hoy ni akhn o
+symbol -> unique 
+object
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
