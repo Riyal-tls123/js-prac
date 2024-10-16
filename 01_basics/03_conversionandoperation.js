569e95382b65670ffb83e964ad137ff06cfbe6e0
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-16 06:07:31 +0000

    conversionnof data types

---
+++ 01_basics/03_conversionandoperation.js
@@ -0,0 +1,19 @@
+let score = 213
+
+console.log(typeof score);
+
+let value = "345"
+
+console.log(typeof value);
+console.log(value)
+
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber);
+console.log(value)
+
+let point = "33abc"
+
+console.log(typeof point);
+let pointInNumber = Number(point)
+console.log(typeof pointInNumber);
+console.log(pointInNumber);
