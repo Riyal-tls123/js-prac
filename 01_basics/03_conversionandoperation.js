a102a4da26facd1bc61c809d1e702f62369617ee
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-16 19:42:33 +0000

    conversion

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -17,3 +17,30 @@ console.log(typeof point);
 let pointInNumber = Number(point)
 console.log(typeof pointInNumber);
 console.log(pointInNumber);
+
+/*
+"33" whn we convert the number it gives "33"->33
+"33abc"->NaN
+true-> 1
+false->0
+*/
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+/*
+now if I convert a empty string "" into boolean
+it will give false; ""->false and "abk"->ture
+*/
+
+let anyNum = 45
+
+let StringNumber = String(anyNum)
+
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+// this is how we convert into string
