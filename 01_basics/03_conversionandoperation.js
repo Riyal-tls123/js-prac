634b8becd10685d157694309ec581a837a397a1d
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-16 21:09:58 +0000

    operations

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -1,22 +1,22 @@
-let score = 213
+// let score = 213
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let value = "345"
+// let value = "345"
 
-console.log(typeof value);
-console.log(value)
+// console.log(typeof value);
+// console.log(value)
 
-let valueInNumber = Number(value)
-console.log(typeof valueInNumber);
-console.log(value)
+// let valueInNumber = Number(value)
+// console.log(typeof valueInNumber);
+// console.log(value)
 
-let point = "33abc"
+// let point = "33abc"
 
-console.log(typeof point);
-let pointInNumber = Number(point)
-console.log(typeof pointInNumber);
-console.log(pointInNumber);
+// console.log(typeof point);
+// let pointInNumber = Number(point)
+// console.log(typeof pointInNumber);
+// console.log(pointInNumber);
 
 /*
 "33" whn we convert the number it gives "33"->33
@@ -25,22 +25,59 @@ true-> 1
 false->0
 */
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 /*
 now if I convert a empty string "" into boolean
 it will give false; ""->false and "abk"->ture
 */
 
-let anyNum = 45
+// let anyNum = 45
 
-let StringNumber = String(anyNum)
+// let StringNumber = String(anyNum)
 
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
 
 // this is how we convert into string
+
+// *********Operations**********
+
+// let value = 3
+
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " Abhra"
+// let str3 = str1 + str2
+
+// console.log(str3); -> Hello Abhra
+
+// console.log("2" + 2); -> 22
+// console.log(2 + "2"); -> 22
+// console.log("2" + 2 + 2); -> 222
+// console.log(2 + 2 + "2"); -> 42
+
+// console.log(2 + "2 + 2"); -> 22 + 2
+// console.log(2 + "2" + 2); -> 222
+
+// so if string is at the beginning thn puro conversion ta string hoy
+// or string jkhn a tar age obdhi ja howar hoy
+
+// let gameCounter = 100
+// gameCounter++; -> 101
+// ++gameCounter; -> 101
+// console.log(gameCounter);
+
