27e75270d91a463bfacbbdfe410f4a027ea33288
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-18 21:16:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+// const name = "Abhra"
+// const repo = 50
+
+// console.log(name + repo + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+const gameName = new String('Abhra-ri-0T')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-9, 4)
+// console.log(anotherString);
+
+const newStringOne = "     Abhra    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://abhra.com/abhra%20kar"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hitesh'));
+console.log(url.includes('abhra'));
+
+console.log(gameName.split('-'));
