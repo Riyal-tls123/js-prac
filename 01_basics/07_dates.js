bda726fa78120a7751ca176039a5f788eb5669f7
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-19 20:18:25 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,28 @@
+// let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+
+// let myCreatedDate = new Date(2024, 0, 23)
+
+//In js 0 represents January
+
+// console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date("2024-01-14")
+
+let myCreatedDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
