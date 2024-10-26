76325ae677c6069460638bd98412ab4227b1b8f8
Riyal-tls123 <abhrakar215@gmail.com>
2024-10-26 13:29:47 +0000

    array

---
+++ 02_basics/array2.js
@@ -0,0 +1,26 @@
+const marvel = ["thor", "Ironman", "Spiderman"]
+const dc = ["Superman", "Flash", "Batman"]
+
+// marvel.push(dc)
+
+// console.log(marvel);
+// console.log(marvel[3][1]);
+
+// const allHeros = marvel.concat(dc)
+// console.log(allHeros);
+
+
+// const allnewHero = [...marvel, ...dc]
+
+// console.log(allnewHero);
+
+// const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const realArray = anotherArray.flat(Infinity)
+
+// console.log(realArray);
+
+console.log(Array.isArray("Abhra"));
+console.log(Array.from);
+
+
